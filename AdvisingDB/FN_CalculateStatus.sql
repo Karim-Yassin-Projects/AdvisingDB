b0ae11ca2b649c83174c9462c4f994780a21b7ba
kareem-elmeteny <kareem.elmeteny@gmail.com>
2023-12-03 00:13:47 +0200

    add docs and missing function

---
+++ AdvisingDB/FN_CalculateStatus.sql
@@ -0,0 +1,22 @@
+﻿CREATE FUNCTION FN_CalculateStatus(
+@studentID INT
+)
+RETURNS BIT
+AS
+BEGIN
+DECLARE @status BIT
+IF EXISTS(SELECT 1
+FROM Installment i
+INNER JOIN 
+Payment p ON i.payment_id = p.payment_id
+WHERE p.student_id = @studentID
+AND i.status = 'notPaid' AND i.deadline > GETDATE())
+BEGIN 
+SET @status = 0
+END
+ELSE
+BEGIN
+SET @status = 1
+END
+RETURN @status
+END
